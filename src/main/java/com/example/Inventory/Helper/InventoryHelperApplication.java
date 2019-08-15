5476093bd6731e86659d4e415be9d6e65b6c01aa
Kelsey <kmiller202011@gmail.com>
2019-08-14 21:01:17 -0500

    This is a hopefully fully functioning repository of my project.  Had to completely reformat my computer and reinstall everything.  Lets make this work.

---
+++ src/main/java/com/example/Inventory/Helper/InventoryHelperApplication.java
@@ -0,0 +1,13 @@
+package com.example.Inventory.Helper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryHelperApplication.class, args);
+	}
+
+}
