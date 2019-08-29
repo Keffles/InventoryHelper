af138f705b3de702e918663a7e4f2c12dce99b40
Kelsey <kmiller202011@gmail.com>
2019-08-28 19:03:38 -0500

    Added controllers for each of the eventual pages for inventory categories.  Added a controller for Inventory page that will eventually display current inventory values.

--- src/main/java/com/example/Inventory/Helper/controllers/Controllers.java
+++ src/main/java/com/example/Inventory/Helper/controllers/Controllers.java
@@ -9,12 +9,62 @@ public class Controllers {
     @RequestMapping(value = "")
     @ResponseBody
     public String index(){
-        return "My first Controller";
+        return "Welcome to inventory helper";
     }
 
-    @RequestMapping(value = "NonRoot")
+    @RequestMapping(value = "Dairy")
     @ResponseBody
-    public String NonRootController(){
-        return "This one is not in the root directory";
+    public String DairyController(){
+        return "Enter the number of milks pulled for each type below";
     }
+
+    @RequestMapping(value = "Pastries")
+    @ResponseBody
+    public String PastriesController(){return "Enter the number of pastries pulled for each value below";}
+
+    @RequestMapping(value = "Coffee")
+    @ResponseBody
+    public String CoffeeController(){return "Enter the number of units pulled for each type of coffee below";}
+
+    @RequestMapping(value = "Cups")
+    @ResponseBody
+    public String CupsController(){return "Enter the number of sleeves pulled for each size of cup below";}
+
+    @RequestMapping(value = "Inclusions")
+    @ResponseBody
+    public String InclusionsController(){return "Enter the number of Inclusions pulled below.";}
+
+    @RequestMapping(value = "Syrups")
+    @ResponseBody
+    public String SyrupsController(){return "Enter the number of Syrups pulled for each type below";}
+
+    @RequestMapping(value = "Refreshers")
+    @ResponseBody
+    public String RefreshersController(){return "Enter the number of refreshers pulled for each type below";}
+
+    @RequestMapping(value = "Oatmeal")
+    @ResponseBody
+    public String OatmealController(){return "Enter the number of oatmeal components pulled below";}
+
+
+    @RequestMapping(value = "Tea")
+    @ResponseBody
+    public String TeaController(){return "Enter the number of teas pulled below.  Iced tea is per box, hot tea bags are per unit";}
+
+    @RequestMapping(value = "Sandwich")
+    @ResponseBody
+    public String SandwichController(){return "Enter the number of Sandwiches, egg bites and wraps pulled below";}
+
+
+    @RequestMapping(value = "Inventory")
+    @ResponseBody
+    public String InventoryController(){return "Below lists current inventory numbers";}
+
+
+
+
+
+
+
+
 }
